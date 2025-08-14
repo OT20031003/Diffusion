fd4a5f326e36f261127ad87f51846937f2e69884
OT20031003 <ono1003takuma@gmail.com>
2025-08-14 09:13:37 +0900

    DDPM separate descr

--- mytest.py
+++ mytest.py
@@ -13,36 +13,6 @@ from DDPM import load_image_as_tensor, save_tensor_as_image, GaussianDiffusion
 
 
 
-        
-def Training(model, optimizer):
-    model.train()
-    dir_path = "./dataset" # dataset内のフォルダの画像
-    epoch = 100
-    sepoch = epoch
-    files_file = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
-    while epoch >= 0:
-        print(f"epoch = {sepoch - epoch}")
-        for x in files_file:
-            optimizer.zero_grad()
-            print(x)
-            x_0 = load_image_as_tensor(dir_path + "/" +x)
-            x_0 = x_0.unsqueeze(0)
-            x_0 = x_0 * 2 - 1
-            maxx = model.timesteps
-            minn = 0
-            t = torch.empty(1).uniform_(minn, maxx).long()
-            print(t)
-            epsilon = torch.randn(x_0.shape)
-            
-            predict = model.unet.forward(model.forward_process(x_0, t, noise=epsilon), t)
-            criterion = torch.nn.MSELoss()
-            loss = criterion(epsilon , predict)
-            print(f"loss = {loss}")
-            loss.backward()
-            optimizer.step()
-        epoch -= 1
-    torch.save(model.state_dict(), 'model_weight.pth')
-
 
 
 
@@ -110,10 +80,7 @@ def InferTest():
     
     save_tensor_as_image(img2.squeeze(0).cpu(), "generated_image.png")
 
-def Training_test():
-    model = GaussianDiffusion()
-    optimizer = torch.optim.Adam(model.parameters(), lr = 0.0001)
-    Training(model, optimizer)
+
 
 
 def main():
